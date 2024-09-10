caaf5ed405afd12496d4e48d3859f7d4ed770883
dejenteklit <dejen.teklit@students.fhnw.ch>
2024-09-10 11:27:30 +0200

    Initial commit for Spring Boot Student Demo

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.util.Calendar.JANUARY;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+           Student mariam = new Student(
+                    "Mariam",
+                    "mariam.gemale@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+            Student alex = new Student(
+
+                    "Alex",
+                    "alex.gemale@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5),
+                    21
+            );
+            //save to our database
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
+
